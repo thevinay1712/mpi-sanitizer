6742216fa3a3bdb0020f5a7ca5b112ff6949f4cd
Vinay Sinnur <vinaysinnur1712@gmail.com>
2026-05-30 23:15:08 +0530

    Updated run.sh to run 5 testcases with timeout guards

--- run.sh
+++ run.sh
@@ -11,23 +11,28 @@ $DIR/mpisan-cxx.sh testcases/test_01_clean.cpp -o test_01
 $DIR/mpisan-cxx.sh testcases/test_03_send_to_self.cpp -o test_03
 $DIR/mpisan-cxx.sh testcases/test_08_async_write_aliasing.cpp -o test_08
 $DIR/mpisan-cxx.sh testcases/test_04_type_mismatch.cpp -o test_04
+$DIR/mpisan-cxx.sh testcases/test_02_null_buffer.cpp -o test_02
 
 echo ""
 echo ">>> RUNNING WORKING CASE: Clean MPI Code (test_01)"
-mpirun -np 2 ./test_01
+timeout 5s mpirun -np 2 ./test_01
 echo "Result: Success (No bugs detected)"
 
 echo ""
 echo ">>> RUNNING FAILURE CASE 1: Deadlock Bug (test_03)"
-mpirun -np 2 ./test_03
+timeout 5s mpirun -np 2 ./test_03
 
 echo ""
 echo ">>> RUNNING FAILURE CASE 2: Buffer Aliasing Bug (test_08)"
-mpirun -np 2 ./test_08
+timeout 5s mpirun -np 2 ./test_08
 
 echo ""
 echo ">>> RUNNING FAILURE CASE 3: Type Mismatch Bug (test_04)"
-mpirun -np 2 ./test_04
+timeout 5s mpirun -np 2 ./test_04
+
+echo ""
+echo ">>> RUNNING FAILURE CASE 4: Null Buffer Bug (test_02)"
+timeout 5s mpirun -np 2 ./test_02
 
 echo ""
 echo ">>> RUNNING LULESH EXASCALE PROXY APP"
